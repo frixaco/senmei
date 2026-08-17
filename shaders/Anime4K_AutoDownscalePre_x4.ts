bbad9f60697563739fbf7a189d7ef35291ee46eb
frixaco <rustam21ashurmatov@gmail.com>
2026-08-18 00:12:03 +0500

    shader math cleanup

--- shaders/Anime4K_AutoDownscalePre_x4.ts
+++ shaders/Anime4K_AutoDownscalePre_x4.ts
@@ -21,19 +21,13 @@ type When = (sizes: { native: Size; output: Size }) => boolean;
 const fragShared = /* wgsl */ `
 @group(0) @binding(0) var frame: texture_2d<f32>;
 @group(0) @binding(1) var frame_sampler: sampler;
-
-fn tex_at(tex: texture_2d<f32>, base_pos: vec4f) -> vec4f {
-  let dims = vec2f(textureDimensions(tex));
-  let uv = base_pos.xy / dims;
-  return textureSampleLevel(tex, frame_sampler, uv, 0.0);
-}
 `;
 
 export const fragF = /* wgsl */ `
 ${fragShared}
 
 @fragment
-fn f(@builtin(position) pos: vec4f) -> @location(0) vec4f {
-  return tex_at(frame, pos);
+fn f(@location(0) uv: vec2f) -> @location(0) vec4f {
+  return textureSampleLevel(frame, frame_sampler, uv, 0.0);
 }
 `;
